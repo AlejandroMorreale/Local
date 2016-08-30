eb0c104fa7642a530e00a7a1f1b68ca25a8355c9
AlejandroMorreale <alee.eme@gmail.com>
2016-08-29 21:42:10 -0300

    Cuarto Commit

--- Rectangulo.php
+++ Rectangulo.php
@@ -1,5 +1,17 @@
 <?php
+require_once('C:\xampp\htdocs\ClaseDos\FigurasGeometricas.php'); 
+
   class Rectangulo {
-	public $ladoDos="2";
+	private $ladoUno;
+	private $ladoDos;
+
+	function Rectangulo($lado1,$lado2){
+		 $this->ladoUno = $lado1;
+		 $this->ladoDos = $lado2;
+	}
+
+	function CalcularDatos(){    	
+     	return $this->ladoUno * $this->ladoDos;    	
+	}
 }
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 require_once('C:\xampp\htdocs\ClaseDos\Rectangulo.php'); 
 
-$MiRectangulo = new Rectangulo();
-echo  $MiRectangulo -> ladoDos;
+$MiRectangulo = new Rectangulo(2,2);
+//echo  $MiRectangulo -> ladoDos;
 
+echo calcularDatos();
 ?>	
